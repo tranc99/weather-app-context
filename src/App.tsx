933e67ac19c0480d322216bdc875179fa3549bf2
Tendai Mutunhire <nomadhideout@yandex.com>
2020-06-22 18:40:45 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm" />
+        <div className="col-sm-8">
+          <h2>Weather App</h2>
+
+          <ol>
+            <li>Component Hierarchy</li>
+            <li>Weather Component</li>
+            <li>Welcome</li>
+            <li>Flag</li>
+            <li>Weather description</li>
+          </ol>
+        </div>
+        <div className="col-sm" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
